dd7ae4b663a31898ae54ce0c8d8e30b8dd729ff2
David Mays <david.mays@outlook.com>
2023-10-20 15:46:26 -0600

    hmm

--- src/main.ts
+++ src/main.ts
@@ -10,162 +10,173 @@
 //YES this is all spaghetti. It was made quickly for tesing..
 
 
-// NOTE: types only partially working.
-// The browser specific version of MQTT is in mqtt/dist/mqtt.min. I'm stealing some types from the NODE version
-// until the browser types are found or fixed. 
+// NOTE: Types not working for browser version (mqtt.min). 
+// I wanted at least the definitions for what IClientOptions and MQTTClient can do,
+// so pulled those from the Node version (lib/client) but that's not a great idea, but we can see
+// the types while developing then turn off the node imports. 
 
 import MqttClient from "mqtt/lib/client"
+import { connect } from "mqtt";
 import { IClientOptions } from 'mqtt/lib/client';
 import * as mqtt from 'mqtt/dist/mqtt.min'
 
+console.log(mqtt)
+let mqtt_client: MqttClient | null = null
 
-let url = "ws://broker.hivemq.com:8000/mqtt";
-let client_id = "clientId-54t4sP6pzs";
-//let test = new m
 
 const subscriptions: any[] = [];
-
-// Press ctrl spacebar in brackets to see options.
-const options: IClientOptions = {
-
-}
-
-
-console.log(mqtt)
-const client: MqttClient = mqtt.connect(url, options);
-
-
-
-
-
-// // const client = mqtt.connect(url, {
-// //     clientId: client_id,
-
-// // });
-
-const client_id_input = document.querySelector("#client_id_input")
-client_id_input.setAttribute("value", client_id)
-client_id_input.addEventListener("input", function (event) { client_id = event.target.value })
-
-const url_input = document.querySelector("#url_input")
-url_input.setAttribute("value", url)
-url_input.addEventListener("input", function (event) { url = event.target.value })
-
-const get_id_button = document.querySelector("#get_id")
-get_id_button.addEventListener("click", function () { window.open("https://www.hivemq.com/demos/websocket-client", "_blank") })
-
-const generate_user = () =>
+const generate_id = () =>
 {
-  var a = ["Impatient", "Blue", "Ugly", "Friendly", "Sunny", "Quick", "Intelligent", "Curious", "Elegant", "Delightful", "Playful", "Vibrant", "Clever", "Lively", "Cozy", "Radiant", "Sparkling", "Adventurous", "Mysterious", "Peaceful"]
-  var b = ["Banana", "Penguin", "Rock", "Pickle", "Noodle", "Giraffe", "Squirrel", "Sock", "Llama", "Wombat", "Pancake", "Unicorn", "Pajamas", "Bear", "Taco", "Tire", "Lobster", "Pineapple", "Kangaroo", "Beluga"];
-  var rA = Math.floor(Math.random() * a.length);
-  var rB = Math.floor(Math.random() * b.length);
-  return a[rA] + b[rB];
+    var a = ["Impatient", "Blue", "Ugly", "Friendly", "Sunny", "Quick", "Intelligent", "Curious", "Elegant", "Delightful", "Playful", "Vibrant", "Clever", "Lively", "Cozy", "Radiant", "Sparkling", "Adventurous", "Mysterious", "Peaceful"]
+    var b = ["Banana", "Penguin", "Rock", "Pickle", "Noodle", "Giraffe", "Squirrel", "Sock", "Llama", "Wombat", "Pancake", "Unicorn", "Pajamas", "Bear", "Taco", "Tire", "Lobster", "Pineapple", "Kangaroo", "Beluga"];
+    var rA = Math.floor(Math.random() * a.length);
+    var rB = Math.floor(Math.random() * b.length);
+    return a[rA] + b[rB];
 }
-const user = generate_user();
-const open_conn_button = document.querySelector("#open_connection");
-const add_sub_button = document.querySelector("#add_subscription");
-const connect_form = document.querySelector("#connect_form");
-const subscribe_form = document.querySelector("#subscribe_form");
-const checkmark = document.querySelector("#checkmark")
+
+const client_id_ui: HTMLSpanElement = document.getElementById("current_user")
+client_id_ui.innerHTML = generate_id()
+const client_id_input: HTMLInputElement = document.getElementById("client_id_input")
+client_id_input.addEventListener("input", (event) =>
+{
+    client_id_ui.innerHTML = event.target.valule;
+});
+// Get the updated value from the input element
+const url_input: HTMLInputElement = document.getElementById("url_input")
+
+// const get_id_button = document.querySelector("#get_id")
+// get_id_button.addEventListener("click", function () { window.open("https://www.hivemq.com/demos/websocket-client", "_blank") })
+
+const open_conn_button = document.getElementById("open_connection");
+const add_sub_button = document.getElementById("add_subscription");
+const connect_form = document.getElementById("connect_form");
+const subscribe_form = document.getElementById("subscribe_form");
+const checkmark = document.getElementById("checkmark")
 // const disconnect_icon = document.querySelector("#disconnect")
 
-const refresh_user = () => document.querySelector("#current_user").innerHTML = user
-const toggle_connect = () =>
+
+const connect_pane_toggle = () =>
 {
-  if (connect_form.hasAttribute("hidden"))
-  {
-    connect_form.removeAttribute("hidden");
-    subscribe_form.setAttribute("hidden", "");
-  } else
-  {
-    connect_form.setAttribute("hidden", "")
-  }
-}
-
-const toggle_subscribe = () =>
-{
-  if (subscribe_form.hasAttribute("hidden"))
-  {
-    subscribe_form.removeAttribute("hidden");
-    connect_form.setAttribute("hidden", "");
-  } else
-  {
-    subscribe_form.setAttribute("hidden", "")
-  }
-}
-
-
-open_conn_button.addEventListener("click", toggle_connect);
-add_sub_button.addEventListener("click", toggle_subscribe);
-
-refresh_user()
-
-const subscribe = (topic) =>
-{
-  client.subscribe(topic, (err) =>
-  {
-    if (err)
+    if (connect_form.hasAttribute("hidden"))
     {
-      alert('Error subscribing to the topic:', err);
+        connect_form.removeAttribute("hidden");
+        subscribe_form.setAttribute("hidden", "");
     } else
     {
-      if (!subscriptions.includes(topic))
-      {
-        subscriptions.push(topic);
-      }
-
-      const dom_subscriptions = document.getElementById("sub_list")
-      const existing_sub = dom_subscriptions.querySelector(`li#${topic}`)
-
-      if (!existing_sub)
-      {
-        const dom_sub = document.createElement("li");
-        dom_sub.id = topic
-
-        const anchor = document.createElement("a");
-        anchor.href = "#";
-        anchor.textContent = topic
-
-        dom_sub.appendChild(anchor);
-        dom_subscriptions.appendChild(dom_sub);
-
-        // now close the subscribe box? no might want to add others.
-      }
+        connect_form.setAttribute("hidden", "")
     }
-  });
+}
+
+const subscribe_pane_toggle = () =>
+{
+    if (subscribe_form.hasAttribute("hidden"))
+    {
+        subscribe_form.removeAttribute("hidden");
+        connect_form.setAttribute("hidden", "");
+    } else
+    {
+        subscribe_form.setAttribute("hidden", "")
+    }
 }
 
 
-client.on("connect", () =>
+open_conn_button.addEventListener("click", connect_pane_toggle);
+add_sub_button.addEventListener("click", subscribe_pane_toggle);
+
+const get_client_inputs = (): [string, IClientOptions] =>
+{
+    const url = url_input.value;
+    const options = {
+        clientId: client_id_input.value
+    }
+
+    return [url, options]
+}
+
+const connect_to_broker = () =>
+{
+    const [url, options] = get_client_inputs();
+    mqtt_client && mqtt_client.end() //Disconnect if client exists.
+    mqtt_client = mqtt.connect(url, options);
+}
+
+connect_to_broker();
+
+mqtt_client.on("connect", () =>
 {
 
-  if (checkmark.hasAttribute("hidden"))
-  {
-    checkmark.removeAttribute("hidden")
-  };
+    if (checkmark.hasAttribute("hidden"))
+    {
+        checkmark.removeAttribute("hidden")
+    };
 
-  //Post username to 
-  client.publish("participants", user)
-  subscribe("participants");
-  client.publish("participants", user)
+    //Post username to 
+    mqtt_client.publish("participants", client_id_input.value)
+    mqtt_client.subscribe("participants");
+    mqtt_client.publish("participants", client_id_input.value)
 });
 
-client.on("disconnect", () =>
+
+const subscribe = (topic: string) =>
+{
+    mqtt_client.subscribe(topic, (err) =>
+    {
+        if (err)
+        {
+            alert('Error subscribing to the topic:' + err);
+        } else
+        {
+            if (!subscriptions.includes(topic))
+            {
+                subscriptions.push(topic);
+            }
+
+            const dom_subscriptions = document.getElementById("sub_list")
+            const existing_sub = dom_subscriptions.querySelector(`li#${topic}`)
+
+            if (!existing_sub)
+            {
+                const dom_sub = document.createElement("li");
+                dom_sub.id = topic
+
+                const anchor = document.createElement("a");
+                anchor.href = "#";
+                anchor.textContent = topic
+
+                dom_sub.appendChild(anchor);
+                dom_subscriptions.appendChild(dom_sub);
+
+                // now close the subscribe box? no might want to add others.
+            }
+        }
+    });
+}
+
+
+
+
+mqtt_client.on("disconnect", () =>
 {
 
-  if (!checkmark.hasAttribute("hidden"))
-  {
-    checkmark.setAttribute("hidden", "")
-  };
+    if (!checkmark.hasAttribute("hidden"))
+    {
+        checkmark.setAttribute("hidden", "")
+    };
 });
 
-client.on("message", (topic, message) =>
+mqtt_client.on("message", (topic, message) =>
 {
-  // message is Buffer
-  console.log(message.toString());
-  client.end();
+    // message is Buffer
+    console.log(message.toString());
+    // mqtt_client.end();
 });
 
+class MqttConnectionManager
+{
+    constructor()
+    {
+
+    }
+}
 
 

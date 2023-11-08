b07cacc315996db5dad80c102340f0683376b91c
David Mays <david.mays@outlook.com>
2023-11-08 12:56:02 -0700

    Revert "working chat inputs"
    This reverts commit bd6191a5c624a768ea81558e9709eb5075b25142.

--- ui_components/_ui.base/elements/dm-chat/dm-chat.ts
+++ ui_components/_ui.base/elements/dm-chat/dm-chat.ts
@@ -2,7 +2,6 @@ import { ISubscriptionGrant, ISubscriptionMap, MqttClient } from "mqtt";
 import DMElement from "../dm-element/dm-element";
 import cssText from "./dm-chat.css?inline";
 import htmlText from "./dm-chat.html?raw";
-import FormInput from "../../../../src/modules/FormInput";
 
 //TODO: containerize subscriptions into each element??
 export default class DMChat extends DMElement {
@@ -15,21 +14,11 @@ export default class DMChat extends DMElement {
         this.classList.add("none");
         this.classList.add("page");
 
-        const topic_btn = document.querySelector(`li[data-topic="${topic_input}"]`) as HTMLLIElement;
         const title = this.shadowRoot?.querySelector("h1") as HTMLHeadingElement;
-        const message_container = this.shadowRoot?.querySelector("section") as HTMLElement;
-        const submit_btn = this.shadowRoot?.querySelector("button") as HTMLButtonElement;
-        const input_text = (): string | undefined => this.shadowRoot?.querySelector("input")?.value;
-
-        const show_self = () => {
-            const pages = document.querySelectorAll(`.page:not([data-topic="${topic_input}"]`);
-            pages.forEach((element) => {
-                element.classList.add("none");
-            });
-            this.classList.remove("none");
-        };
-
         title.innerHTML = topic_input;
+
+        const message_container = this.shadowRoot?.querySelector("section") as HTMLElement;
+
         // would be better to run this on topics_granted, for accurate QoS levels granted.
         mqtt_client!.on("message", (topic, message) => {
             if (topic == topic_input) {
@@ -39,14 +28,19 @@ export default class DMChat extends DMElement {
             }
         });
 
-        topic_btn.addEventListener("click", (event) => {
-            event.stopPropagation();
+        const show_self = () => {
+            const pages = document.querySelectorAll(`.page:not([data-topic="${topic_input}"]`);
+            pages.forEach((element) => {
+                element.classList.add("none");
+            });
+            this.classList.remove("none");
+        };
+
+        const topic_button = document.querySelector(`li[data-topic="${topic_input}"]`) as HTMLLIElement;
+        topic_button.addEventListener("click", (event) => {
+            // alert(event.target.getAttribute("data-topic"));
             console.log(topic_input + "|" + this.getAttribute("data-topic"));
             show_self();
-        });
-
-        submit_btn.addEventListener("click", (event) => {
-            mqtt_client.publish(topic_input, input_text());
         });
     }
 }
